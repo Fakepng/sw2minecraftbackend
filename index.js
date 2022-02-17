9df47174151553774107c8479f8a3fce673a493a
Fakepng <86796781+Fakepng@users.noreply.github.com>
2022-02-17 14:30:31 +0700

    Minecraft backend

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+const Query = require("minecraft-query");
+const mineQuery = new Query({host: process.env.SERVER, port: process.env.QUERYPORT, timeout: 7500});
+
+app.get("/minecraft", (req, res) => {
+    mineQuery.fullStat()
+    .then(success => {
+        res.json(success)
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
