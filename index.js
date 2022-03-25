2372fd053c7eeb06cd5b797aeffc730bc88ab39a
Fakepng <krit0kasemtewin@gmail.com>
2022-03-26 04:27:36 +0700

    Refactor and Add
    Refactor: CORS
    Add: user for sw2minecraft register website

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 5050;
+const CLIENT = process.env.CLIENT || '*';
+const DATABASE = process.env.DATABASE;
 const app = express();
 
+app.use(bodyParser.json());
+
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', CLIENT);
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     res.setHeader('Access-Control-Allow-Credentials', true);
@@ -12,12 +18,21 @@ app.use(function (req, res, next) {
 });
 
 app.get("/", (req, res) => {
-    res.json({ message: "welecome to fakepng api backend", contact: "contact@fakepng.com"})
+    res.json({ message: "welecome to fakepng api backend", contact: "contact@fakepng.com" })
 })
 
-const minecraftRouter = require('./routes/minecraft');
-app.use('/minecraft', minecraftRouter);
+app.use('/minecraft', require('./routes/minecraft'));
+app.use('/user', require('./routes/user'));
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
+});
+
+mongoose.connect(DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log("Connected to Database 💽");
+}).catch((err) => {
+    console.log(err)
 });
